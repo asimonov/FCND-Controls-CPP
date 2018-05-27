257f12a6650f554b5d412ed9a0ef5103c21feb4a
Alexey <alexey.simonov@gmail.com>
2018-05-27 17:16:07 +0100

    fixing python formatting

--- config/traj/MakePeriodicTrajectory.py
+++ config/traj/MakePeriodicTrajectory.py
@@ -22,14 +22,17 @@ with open('FigureEight.txt', 'w') as the_file:
         y = math.sin(t * 2 * math.pi / period[1] + phase[1]) * radius * amp[1] + center[1];
         z = math.sin(t * 2 * math.pi / period[2] + phase[2]) * radius * amp[2] + center[2];
         the_file.write(fmt(t) + "," + fmt(x) + "," + fmt(y) + "," + fmt(z));
-		vx = 0;
-		vy = 0;
-		vz = 0;
-		######## BEGIN STUDENT CODE
-		
+        ######## BEGIN STUDENT CODE
+        vx = (x-px)/timestep;
+        vy = (y-py)/timestep;
+        vz = (z-pz)/timestep;
+        px = x
+        py = y
+        pz = z
+
 		######## END STUDENT CODE
-		the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz));
-		######## EXAMPLE SOLUTION
+        the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz));
+        ######## EXAMPLE SOLUTION
         #the_file.write("," + fmt((x-px)/timestep) + "," + fmt((y-py)/timestep) + "," + fmt((z-pz)/timestep));
 		#px = x;
         #py = y;
